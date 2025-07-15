9741d56601f1e006605f8a0a5b7c0f2ea73d7d7c
Sawari Jamgaonkar <122595291+Sawarijamgaonkar@users.noreply.github.com>
2025-07-15 08:10:34 +0530

    Delete jest.config.js

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-export default {
-    transform: {
-        '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest'
-    },
-    moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json'],
-    testEnvironment: 'jsdom',
-    extensionsToTreatAsEsm: ['.ts', '.tsx']
-};
